417209752af2d183b03679c327950c0d28efc852
Unknown <msommersevtech@gmail.com>
2018-01-22 08:43:16 -0700

    Committing first version of assgn 1

---
+++ DGM 1600/McKenna-Sommers-assign1/color.js
@@ -0,0 +1,8 @@
+
+var firstName = prompt('Hello! What is your first name?');
+
+var lastName = prompt('Okay. What is your last name?');
+
+var favColor = prompt('Cool. What is your favorite color?');
+
+alert('Your name is ' + firstName + ' ' + lastName + ' and your favorite color is ' + favColor);
